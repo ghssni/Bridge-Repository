defc4c96fa3c0a3d0765af837313a35d3ee65370
Ghassani Nurbaningtyas <ghtyas@MacBook-Air-Ghassani.local>
2025-02-06 00:42:32 +0700

    add adrive app script

---
+++ drive_apps_script.js
@@ -0,0 +1,22 @@
+'''
+Script ini digunakan untuk otomasi pembuatan folder pada url google drive 
+sesuai nama tim yang ada pada table spreadsheet
+
+Copy code ini dan jalankan pada app script di spreadsheet:
+    - Extension -> apps script
+'''
+
+function createFoldersFromSheet() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var folderNames = sheet.getRange("B2:B48").getValues(); // Sesuaikan rentang data letak nama tim
+
+  var parentFolder = DriveApp.getFolderById("");
+
+  folderNames.forEach(function (name) {
+    if (name[0]) {
+      parentFolder.createFolder(name[0]);
+    }
+  });
+
+  Logger.log("Folders created successfully in the specific folder!");
+}
